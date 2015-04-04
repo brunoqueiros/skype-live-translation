137c8602400013d3fc2ce3d253c043f5e46b6349
Bruno Queiros <brunoofq@gmail.com>
2015-04-04 18:03:44 -0300

    add jscs and jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,23 @@
-'use strict';
 var gulp  = require('gulp');
-var source = require('vinyl-source-stream');
-var browserify = require('gulp-browatchify');
-var reactify = require('6to5ify') // reactify transform
-
-// gulp.task('browserify', function () {
-//   gulp.src('./app/src/main.js')
-//     .pipe(browserify({
-//       debug: !process.env.production,
-//       transforms:[reactify]
-//     }))
-//     .pipe(source('bundle.js'))
-//     .pipe(gulp.dest('./app/dist'))
-// })
-// gulp.watch('./app/src/**/*.js', ['browserify']);
-
-// var mocha = require('gulp-mocha');
- 
-// gulp.task('test', function () {
-//     return gulp.src('./test/util.js', {read: false})
-//         .pipe(mocha({reporter: 'nyan'}));
-// });
-
 var babel = require('gulp-babel');
- 
-gulp.task('default', function () {
-    return gulp.src('app/src/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('dist/src/'));
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+
+const SOURCE = ['app/src/**/*.js'];
+
+gulp.task('babel', function () {
+  return gulp.src(SOURCE)
+    .pipe(babel())
+    .pipe(gulp.dest('dist/src/'));
 });
 
-// gulp.task('default', ['browserify']);
-
-// 'use strict';
-// var gulp  = require('gulp');
-// var babelify = require('babelify');
-// var browserify = require('browserify');
-// var source = require('vinyl-source-stream');
-
-// gulp.task('browserify', function() {
-//     browserify({
-//     entries: './app/src/main.js',
-//     debug: true
-//     })
-//     .transform(babelify)
-//     .bundle()
-//     .pipe(source('output.js'))
-//     .pipe(gulp.dest('./dist'));
-// });
-
-// gulp.task('default', ['browserify']);
+gulp.task('jscs', function () {
+  return gulp.src(SOURCE)
+    .pipe(jscs());
+});
+ 
+gulp.task('lint', function() {
+  return gulp.src(SOURCE)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
